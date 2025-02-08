5a9e25537deec4d4feea6786bc7466735eac8841
davidheang <2023017heang@aupp.edu.kh>
2025-02-08 11:43:33 +0700

    commit changes on var

--- assignment-1.js
+++ assignment-1.js
@@ -8,17 +8,25 @@ async function scrapeWebsite(url) {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
 
-    const titles = [];
-    // loop through all article elements with product_pod class and extract h3 element
+    const prices = [];
+
+    // loop through all article elements with product_pod class and extract class priduct_price
     // if you to scrape other site you need to change the class name and the selector
     // since all site are built differently
-    $("article.product_pod h3 a").each((index, element) => {
-        // push the title attribute of anchor element
-      const title = $(element).attr("title");
-      titles.push(title);
+    $("article.product_pod div.product_price p.price_color").each((index, element) => {
+      //push the price to the array prices
+      const price = $(element).text().trim();
+      prices.push(price);
     });
-
-    console.log("Extracted Titles:", titles);
+    
+    // filter product price
+    const cleanedPrices = prices.map(price => price.replace('£', ''));
+    // show only product less 30 dollars
+    for(let i = 0; i < cleanedPrices.length; i++) {
+      if(cleanedPrices[i] < 30){
+        console.log(cleanedPrices[i])
+      }
+    }
   } catch (error) {
     //display error message
     console.error("Error fetching the website:", error.message);
